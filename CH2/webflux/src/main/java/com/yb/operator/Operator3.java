8a1473e9d3e39054cb068ce14645887b6945e56c
albin <dydqls5757@gmail.com>
2024-07-16 17:32:19 +0900

    reactor

--- CH2/webflux/src/main/java/com/yb/operator/Operator3.java
+++ CH2/webflux/src/main/java/com/yb/operator/Operator3.java
@@ -1,13 +1,36 @@
 package com.yb.operator;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
 public class Operator3 {
+    // count
+    public Mono<Long> fluxCount() {
+        return Flux.range(1, 10)
+                .count()
+                .log();
+    }
 
-    //count
+    // distinct
+    public Flux<String> fluxDistinct() {
+        return Flux.fromIterable(List.of("a", "b", "a", "b", "c"))
+                .distinct();
+    }
 
-    //distinct
+    // reduce
+    public Mono<Integer> fluxReduce() {
+        return Flux.range(1,10)
+                .reduce((i, j) -> i+j)
+                .log();
+    }
 
-    //reduce
-
-    //groupby
-
+    // groupby
+    public Flux<Integer> fluxGroupBy() {
+        return Flux.range(1,10)
+                .groupBy(i -> (i % 2 == 0) ? "even" : "odd")
+                .flatMap(group -> group.reduce((i, j) -> i+j))
+                .log();
+    }
 }
---
+++ CH2/webflux/src/test/java/com/yb/operator/Operator4Test.java
@@ -0,0 +1,25 @@
+package com.yb.operator;
+
+import com.yb.operator.Operator4;
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Operator4Test {
+    private Operator4 operator4 = new Operator4();
+
+    @Test
+    void fluxDelayAndLimit() {
+        StepVerifier.create(operator4.fluxDelayAndLimit())
+                .expectNext(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                .verifyComplete();
+    }
+
+    @Test
+    void fluxSample() {
+        StepVerifier.create(operator4.fluxSample())
+                .expectNextCount(1000)
+                .verifyComplete();
+    }
+}
